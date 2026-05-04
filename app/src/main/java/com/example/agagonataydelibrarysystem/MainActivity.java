59daa6267d80593cedb0e9b9473327a84bc63c35
Tj Atayde <tjatayde@gmail.com>
2026-05-04 22:19:49 +0800

    initialized components

--- app/src/main/java/com/example/agagonataydelibrarysystem/MainActivity.java
+++ app/src/main/java/com/example/agagonataydelibrarysystem/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.agagonataydelibrarysystem;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText bookDetails, bookID;
+
+    ListView bookList;
+    Button addBook, viewBook, updateBook, deleteBook, clearBook;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        init();
+    }
+    public void init(){
+
+        bookDetails = findViewById(R.id.etBookDetail);
+        bookID = findViewById(R.id.etBookID);
+        bookList = findViewById(R.id.lvBookList);
+        addBook = findViewById(R.id.btnAdd);
+        viewBook = findViewById(R.id.btnView);
+        updateBook = findViewById(R.id.btnUpdate);
+        deleteBook = findViewById(R.id.btnDelete);
+        clearBook = findViewById(R.id.btnClear);
+
     }
 }
